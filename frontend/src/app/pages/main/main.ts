5a8f05d873d3cf937d906570d20f60de647e2a03
Autumn <autumn@example.com>
2026-04-18 00:52:30 +0500

    temporary changes in main component

--- frontend/src/app/pages/main/main.ts
+++ frontend/src/app/pages/main/main.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {
+export class Main implements OnInit {
+  movies: any[] = [];
+  filteredMovies: any[] = [];
+  searchTitle: string = '';
+  loading = false;
+  error = '';
 
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    this.loading = true;
+    this.movieService.getMovies().subscribe({
+      next: (data: any) => {
+        this.movies = data;
+        this.filteredMovies = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Ошибка загрузки';
+        this.loading = false;
+        console.error(err);
+      }
+    });
+  }
+
+  applyFilters() {
+    let filtered = [...this.movies];
+    if (this.searchTitle) {
+      filtered = filtered.filter(m => m.title.toLowerCase().includes(this.searchTitle.toLowerCase()));
+    }
+    this.filteredMovies = filtered;
+  }
+
+  onSearchChange() {
+    this.applyFilters();
+  }
 }
