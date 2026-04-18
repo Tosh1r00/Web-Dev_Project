96b184fbe12e2e7319d8719a034e5719333aae55
Tosh1r00 <toshiro@MacBook-Air-Anatoliy.local>
2026-04-18 23:56:30 +0500

    Made Registor/Login(Auth) serializers

---
+++ backend/users_login/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import User
+
+#создаем нового пользователя
+class RegistorSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True) #не возвращаем пароль, только принимаем
+
+    class Meta:
+        model = User
+        fields = ['username' , 'email' , 'password']
+
+        def create(self, validated_data):
+            user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+            return user 
+        
+#для авторизованных пользователей просто принимаем вводные
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
