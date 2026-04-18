81aa378db0f226742b7dbb7bbd68c8918dcc645d
Autumn <autumn@example.com>
2026-04-19 02:51:46 +0500

    update routes to use homepage component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Main } from './pages/main/main';
+import { HomePage } from './pages/home-page/home-page';
+
 export const routes: Routes = [
-  {path: '', component: Main},
+  {path: '', component: HomePage},
   {path: '**', redirectTo: ''},
 ];
