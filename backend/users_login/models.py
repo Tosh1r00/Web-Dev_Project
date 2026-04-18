61c91c757c04d67eeb7f4ff3388f166a721e3f7a
Tosh1r00 <toshiro@MacBook-Air-Anatoliy.local>
2026-04-18 23:47:20 +0500

    Generated User(model) + updated settings to reassign model + makemigrations

--- backend/users_login/models.py
+++ backend/users_login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser #встроенная модель юзера(ник + пароль)
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True) #добавляем уникальный имейл для пользователя
+
+    def __str__(self):
+        return self.username
+    
