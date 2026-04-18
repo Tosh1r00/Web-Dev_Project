6a0e1f0b44bb54cdad5ccd43595103380227e9b7
Autumn <autumn@example.com>
2026-04-18 23:06:52 +0500

    implement main component

--- frontend/src/app/pages/main/main.ts
+++ frontend/src/app/pages/main/main.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MovieService } from '../../services/movie.service';
-import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-main',
