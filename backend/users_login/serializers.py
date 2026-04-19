27a13b756a1a72b4812721a2ddb3a51c72dff7bf
Tosh1r00 <toshiro@MacBook-Air-Anatoliy.local>
2026-04-19 20:44:41 +0500

    Trying to fix bugs with GIT main

--- backend/users_login/serializers.py
+++ backend/users_login/serializers.py
@@ -11,9 +11,9 @@ class RegistorSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-        username=validated_data['username'],
-        email=validated_data['email'],
-        password=validated_data['password'],
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
     )
         return user 
         
