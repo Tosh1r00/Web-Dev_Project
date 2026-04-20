af28d1f61c30e281dc2eeb3b932d29781c8cd05d
Autumn <autumn@example.com>
2026-04-20 18:40:46 +0500

    register Genre, Movie, Hall, Session, Booking in admin panel

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Genre, Movie, Hall, Session, Booking
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Hall)
+admin.site.register(Session)
+admin.site.register(Booking)
