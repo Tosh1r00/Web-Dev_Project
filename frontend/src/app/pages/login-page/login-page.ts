b62c55c24eaa2487aacf344955b07a30191f9aef
Anatoliy Kim <ana_kim@kbtu.kz>
2026-04-20 18:58:54 +0500

    Logic for Login-form

--- frontend/src/app/pages/login-page/login-page.ts
+++ frontend/src/app/pages/login-page/login-page.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //для инпут [(ngModel)]
+import { Router } from '@angular/router'; //для маршрутизации после AUTH/REG
+import { CommonModule } from '@angular/common'; //для ngIf and NgFor 
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  //Переменные которые вешаю на инпуты:
+  student_id: string = '';
+  password: string = '';
+  username: string = '';
 
+  //Логика которую я буду использовать для переключения между AUTH/REG:
+  isLogin: boolean = true;
+
+  //Для формы с ошибками
+  errorMessage: String = ''
+
+  // for dependency-injection + сервис подключаем
+  constructor (private router: Router){}
+
+  //Для переключение Reg <-> Auth
+  changeMode(){
+    this.isLogin = !this.isLogin;
+    this.errorMessage = ''
+  }
+
+  // Отправка формы - REG/AUTH
+  onSubmit() {
+    if (this.isLogin) {
+      this.login();
+    } else {
+      this.register();
+    }
+  }
+
+  login() {
+    console.log('Логин:', this.student_id, this.password);
+  }
+
+  register() {
+    console.log('Регистрация:', this.username, this.student_id, this.password);
+  }
 }
