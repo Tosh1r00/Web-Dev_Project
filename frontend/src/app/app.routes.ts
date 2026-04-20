80116e5022dccb478a63530e691d4cf6fc9089f3
Tosh1r00 <ana_kim@kbtu.kz>
2026-04-20 23:56:30 +0500

    Added a path to login page using Router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
+import { LoginPage } from './pages/login-page/login-page'; // import LoginPage
 
 export const routes: Routes = [
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
   { path: 'movies', component: HomePage },
-  { path: '**', redirectTo: 'movies' },
+  { path: 'login', component: LoginPage },
+
+  { path: '**', redirectTo: 'movies' }, //все страницы перенаправляют на мувис
 ];
