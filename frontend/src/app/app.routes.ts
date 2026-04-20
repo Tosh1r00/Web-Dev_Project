4524d477a30e0fa05214a5ba98429d53f868b163
Autumn <autumn@example.com>
2026-04-20 18:41:47 +0500

    replace root route with redirect to /movies and add movies route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { Main } from './pages/main/main';
 import { HomePage } from './pages/home-page/home-page';
 
 export const routes: Routes = [
-  {path: '', component: HomePage},
-  {path: '**', redirectTo: ''},
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'movies', component: HomePage },
+  { path: '**', redirectTo: 'movies' },
 ];
