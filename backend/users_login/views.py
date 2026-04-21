77b30ac88f13ac4560cd54c10eff5fce79d03ebc
Tosh1r00 <ana_kim@kbtu.kz>
2026-04-21 05:59:55 +0500

    Created a CBV for API hadling for AUTH.users to get/patch data aand recieve there History

--- backend/users_login/views.py
+++ backend/users_login/views.py
@@ -1,9 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
 # Регистрация - сериалайзером проверяем тру ли данные при вводе
 @api_view(['POST']) # проверяем запрос
@@ -21,12 +23,12 @@ def register(request):
 def login(request):
     serializer = LoginSerializer(data=request.data)
     if serializer.is_valid():
-        user = authenticate(
+        user = authenticate( # authenticate ищет юзера в базе по student_id и password
             student_id=serializer.validated_data['student_id'],
             password=serializer.validated_data['password']
         )
         if user:
-            refresh = RefreshToken.for_user(user)
+            refresh = RefreshToken.for_user(user) # Создаём JWT токены для найденного пользователя
             return Response({
                 'access': str(refresh.access_token), # основной токен, который используется для аутентификации пользователя при отправке запросов
                 'refresh': str(refresh),
@@ -49,3 +51,36 @@ def logout(request):
         return Response(
             {'error': 'Неверный токен'}, status=status.HTTP_400_BAD_REQUEST
         )
+    
+# For profile-page:
+# Обработка запросов 
+class UserView(APIView):
+    # IsAuthenticated — только залогиненные могут обращаться
+    permission = [IsAuthenticated]
+
+    def get(self,request):
+        serializer = UserSerializer
+        return Response(serializer.data)
+    
+    def patch(self,request):
+         # partial=True — можно обновить только часть полей
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# История бронирований текущего пользователя
+# GET /api/users/bookings/ — список всех броней юзера
+
+class UserHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        from movies.models import Booking
+        from movies.serializers import BookingSerializer
+
+        #Filter only by ID user
+        bookings = Booking.objects.filter(user = request.user).order_by('-created_at')
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
