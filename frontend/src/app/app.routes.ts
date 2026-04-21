9fdf65f5a6591db69ee937a3e097e8a848a2816f
Tosh1r00 <ana_kim@kbtu.kz>
2026-04-21 07:05:29 +0500

    Added routing for Profil by /profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { LoginPage } from './pages/login-page/login-page'; // import LoginPage
+import { ProfilePage } from './pages/profile-page/profile'; // import ProfilePage
 
 export const routes: Routes = [
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
   { path: 'movies', component: HomePage },
   { path: 'login', component: LoginPage },
-
+  { path: 'profile', component: ProfilePage},
   { path: '**', redirectTo: 'movies' }, //все страницы перенаправляют на мувис
 ];
