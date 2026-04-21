466dcf5e699c5a9972cda4b372af15910c895649
Tosh1r00 <ana_kim@kbtu.kz>
2026-04-21 07:45:46 +0500

    Added interceptor into HTTP request

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,5 @@
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
@@ -7,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ]
 };
