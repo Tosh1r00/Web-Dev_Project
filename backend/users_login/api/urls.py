557ffbdfc0fc04412176733f90e7143109395355
Tosh1r00 <ana_kim@kbtu.kz>
2026-04-21 06:21:31 +0500

    Added user profile to urls

--- backend/users_login/api/urls.py
+++ backend/users_login/api/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from .. import views
 
 urlpatterns = [
+    # FBV routers for login-page
     path('register/', views.register),
     path('login/', views.login),
     path('logout/', views.logout),
+
+    #CBV routers for user-page
+    path ('users/me/', views.UserView.as_view()),
+    path ('users/history/', views.UserHistoryView.as_view()),
 ]
